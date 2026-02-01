6e1a24ea2afcdcff70606e6ee70eb8931c0ba098
Shr3ee <rajshreebhuyan1306@gmail.com>
2026-02-01 16:46:18 +0530

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Rajshree Bhuyan")
+let students={n:"hari" , age:22 , course:"CSE"};
+console.table(students);
+console.table(["apple", 12 , "Ece"])
+// console.clear()
+console.time("test");
+for(let i=0;i<10000;i++){}
+console.timeEnd("test");
+console.group("cse24");
+console.log("name:prip");
+console.groupEnd("cse24");
+console.count("click");
+console.count("click");
+console.count("click");
+console.count("click");
+console.count("click");
